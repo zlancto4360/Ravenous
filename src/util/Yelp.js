55d88297d394e1e9ba1f94f0b75d8892e58769ad
zlancto4360 <39075641+zlancto4360@users.noreply.github.com>
2018-05-07 23:38:10 -0400

    Add files via upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,29 @@
+const apiKey = 'y-qNdl3waR_28wJMbV9HloBwXiFzGUrMWxC3Zf-r8K-8kb46j2SBZZ6KgsTf-9aTG5tI2PsBVLT3P3DPmgdk15kn2olwwLWJMyJgkQCA0bFUtwmswmVSstsHBgnxWnYx';
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response =>{
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+}
+
+export default Yelp;
